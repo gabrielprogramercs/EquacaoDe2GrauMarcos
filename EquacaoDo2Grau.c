cd72bde38b1ec939f417e9cdbd3dbc37e3dc94a2
Gabriel Lopes <gabriel-fernando2008@hotmail.com>
2026-04-14 14:11:20 -0300

    Create EquacaoDo2Grau.c

---
+++ EquacaoDo2Grau.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    float a, b, c, delta, x1, x2;
+
+    printf("Digite os coeficientes a, b e c da equacao do 2 grau (ax2 + bx + c = 0): ");
+    scanf("%f %f %f", &a, &b, &c);
+
+    if (a == 0) {
+        printf("Nao é uma equacao do 2º grau.\n");
+        return 0;
+    }
+
+    delta = b * b - 4 * a * c;
+
+    if (delta < 0) {
+        printf("A equacao nao possui raizes reais.\n");
+    } else if (delta == 0) {
+        x1 = -b / (2 * a);
+        printf("A equacao possui uma raiz real: x = %.2f\n", x1);
+    } else {
+        x1 = (-b + sqrt(delta)) / (2 * a);
+        x2 = (-b - sqrt(delta)) / (2 * a);
+        printf("A equacao possui duas raizes reais: x1 = %.2f e x2 = %.2f\n", x1, x2);
+    }
+
+    return 0;
+}
